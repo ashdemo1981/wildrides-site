f646953d479df3772a534fb05cfc72945ca99ea4
ashdemo1981 <60956852+ashdemo1981@users.noreply.github.com>
2026-03-01 20:19:00 -0700

    Update Cognito user pool configuration

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_d0SS8zb0K', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4dd4043fs5737bisb0t08ipt8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_eCGOR83Lx', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '77s5q8k66o3a8oj92rtath9b7b', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
