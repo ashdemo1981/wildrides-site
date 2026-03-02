cb44b508a31ed3cf8ce68ed9ba106f0e5ef0ad0b
ashdemo1981 <60956852+ashdemo1981@users.noreply.github.com>
2026-03-01 20:36:11 -0700

    Update userPoolClientId in config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_eCGOR83Lx', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '77s5q8k66o3a8oj92rtath9b7b', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '7oabn5o6fojtp0888jfjkt3n65', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
